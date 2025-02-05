bfc2abcccb4e5afc74985d73901844357929839c
Arianhh <arianhaghighi732@gmail.com>
2025-02-05 20:58:09 +0330

    Change all components to functional components

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,26 +1,15 @@
-import { Component } from "react";
 import Card from "../card/card.component"
 
 import "./card-list.style.css";
 
-class CardList extends Component {
-    constructor() {
-        super();
-    }
-
-
-    render() {
-        const { monsters, className} = this.props
-        return (
-            <div className={`card-list ${className}`}>
-                {
-                    monsters.map(monster => (
-                        <Card monster={monster} imgAlt={monster.name} />
-                    ))
-                }
-            </div>
-        );
-    }
-}
+const CardList = ({ monsters, className }) => (
+    <div className={`card-list ${className}`}>
+        {
+            monsters.map(monster => (
+                <Card monster={monster} imgAlt={monster.name} />
+            ))
+        }
+    </div>
+)
 
 export default CardList;
--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -1,20 +1,18 @@
-import { Component } from "react";
-
 import "./card.style.css";
 
-class Card extends Component {
 
-    render() {
-        const { name, email, id } = this.props.monster;
-        const { imgAlt, className } = this.props;
-        return (
-            <div className={`card-container ${className}`} key={id}>
-                <img src={`https://robohash.org/${id}?set=set2&size=200x200`} alt={imgAlt} />
-                <h2>{name}</h2>
-                <p>{email}</p>
-            </div>
-        );
-    }
+
+
+const Card = ({ monster, imgAlt, className }) => {
+    const { name, email, id } = monster;
+    return (
+        <div className={`card-container ${className}`} key={id}>
+            <img src={`https://robohash.org/${id}?set=set2&size=200x200`} alt={imgAlt} />
+            <h2>{name}</h2>
+            <p>{email}</p>
+        </div>
+    );
+
 }
 
 export default Card;
